8f7a229e1b7a2550f6d1c1e3af6fd63f68b34e86
SteelerFroz987 <jubile.vanaa1@gmail.com>
2021-06-07 09:06:02 -1000

    maj

--- class/classlangue.php
+++ class/classlangue.php
@@ -2,7 +2,7 @@
     include __DIR__ .'/../connexion/connexion.php';
 
 class langue {
-
+    // Permet de créer une langue
     public function createLangue($name,$translate){
         global $conn;
         $request_insert = "INSERT INTO `langue` (`name`,`translate`) VALUES ('" . $name . "','" . $translate . "')";
@@ -13,20 +13,21 @@ class langue {
     public function getAllLangue(){
         global $conn;
 
-        // je récupère liste manga
+        // Je récupère les langues
         $request_all = "SELECT * FROM `langue`";
         $get_all_langue = $conn->query($request_all);
 
         return $get_all_langue;
     }
 
+    // Supprime la langue sélectionner
     public function deleteLangue($id){
         global $conn;
 
         $delete_langue_request = "DELETE FROM `langue` WHERE id=".$id;
         $conn->query($delete_langue_request);
     }
-
+    // Met à jour la langue sélectionner
     public function updateLangue($id,$name,$translate){
         global $conn;
         $update = "UPDATE `langue` SET `name`='".$name."',`translate`='".$translate."' WHERE `id`=".$id;
